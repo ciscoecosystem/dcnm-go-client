6b3e91242cfb483b5a7899747ed9fb9691ffc5ba
AshuSoni-crest <ashutosh.soni@crestdatasys.com>
2020-12-10 14:28:56 +0530

    client update

--- client/client.go
+++ client/client.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"crypto/tls"
 	b64 "encoding/base64"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -183,9 +182,16 @@ func (c *Client) authenticate() error {
 	}
 	req.Header.Set("Authorization", fmt.Sprintf("Basic %s", getBasicAuth(c.username, c.password)))
 
-	obj, _, err := c.do(req)
+	obj, resp, err := c.do(req)
 	if err != nil {
-		return errors.New("Invalid username or password")
+		return err
+	}
+	if resp.StatusCode == 500 {
+		bodybytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf(string(bodybytes))
 	}
 
 	token := models.StripQuotes(obj.S("Dcnm-Token").String())
